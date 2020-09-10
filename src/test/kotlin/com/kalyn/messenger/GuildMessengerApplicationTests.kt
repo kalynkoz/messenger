79d19cd8717033e62150b869388ff58dde1f794b
Kalyn Eshleman <kalynkoz@yahoo.com>
2020-09-10 15:49:52 -0600

    initial commit with basic setup

---
+++ src/test/kotlin/com/kalyn/messenger/GuildMessengerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kalyn.messenger
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GuildMessengerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
