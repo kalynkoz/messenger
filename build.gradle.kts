398b6d889d4672b9f00aa757b79c620ed0aa07be
Kalyn Eshleman <kalynkoz@yahoo.com>
2020-09-13 12:27:08 -0600

    removing exclusion

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,9 +20,7 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	testImplementation("org.springframework.boot:spring-boot-starter-test") {
-		exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
-	}
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
 
 	testImplementation("org.junit.jupiter:junit-jupiter:5.5.2")
 	testImplementation("io.mockk:mockk:1.10.0")
